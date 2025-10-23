9a53601b0d8b4ee4c0e3610030b5338f8101b46e
LAI Jingcheng <jingcheng.lai@marinesphere.jp>
2025-10-23 22:28:57 +0900

    Use HashRouter and rebuild static site

--- global-perspectives-starter/frontend/src/App.jsx
+++ global-perspectives-starter/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
 import WorldMap from './components/WorldMap';
@@ -6,14 +6,14 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/map" element={<WorldMap />} />
         </Routes>
       </Layout>
-    </Router>
+    </HashRouter>
   );
 }
 
